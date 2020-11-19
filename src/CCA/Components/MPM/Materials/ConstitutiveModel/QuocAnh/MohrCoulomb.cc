7dac733c3b3f2b5052d7c2978622b405967723de
Quoc Anh Tran <quoct@ntnu.no>
2020-11-19 12:42:38 +0100

    fix

--- src/CCA/Components/MPM/Materials/ConstitutiveModel/QuocAnh/MohrCoulomb.cc
+++ src/CCA/Components/MPM/Materials/ConstitutiveModel/QuocAnh/MohrCoulomb.cc
@@ -631,6 +631,10 @@ double rho_orig = matl->getInitialDensity();
               base_friction = 0.1706 * pow((0.55416 + 2 * (U - 0.3126 * 0.3126 * tan(80 * 3.1415 / 180)) - (U - 0.3126) * (U - 0.3126) * tan(30 * 3.1415 / 180)),-0.336);                 
           }
 
+          if (base_friction > 0.89)
+          {
+              base_friction = 0.89;
+          }
           pMunew[idx] = base_friction;
           sigarg[53] = base_friction;
 
